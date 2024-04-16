Effect-TS/effect

Either: fix `getEquivalence` parameter order from `Either.getEquivale… (#2243)

packages/schema/src/Schema.ts
@@ -4029,7 +4029,7 @@ export const eitherFromSelf = <E, IE, RE, A, IA, RA>({ left, right }: {
       description: `Either<${format(left)}, ${format(right)}>`,
       pretty: eitherPretty,
       arbitrary: eitherArbitrary,
-      equivalence: Either.getEquivalence
+      equivalence: (left, right) => Either.getEquivalence({ left, right })
     }
   )
 }

billiondev-911/tail1

packages/schema/src/Schema.ts
@@ -5339,7 +5339,7 @@ export const EitherFromSelf = <R extends Schema.Any, L extends Schema.Any>({ lef
       description: `Either<${format(right)}, ${format(left)}>`,
       pretty: eitherPretty,
       arbitrary: eitherArbitrary,
-      equivalence: (right, left) => either_.getEquivalence(left, right)
+      equivalence: (right, left) => either_.getEquivalence({ left, right })
     }
   )
 }